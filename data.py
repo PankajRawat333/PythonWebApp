e578eb6fa41b4bfa4a4f268faf1c2a5f263f0abb
SAPIENT\prawa5 <pankaj.rawat@sapient.com>
2018-02-22 15:18:13 +0530

    second commit

---
+++ data.py
@@ -0,0 +1,36 @@
+def Articles():
+    Articles = [{
+        'id':
+        1,
+        'title':
+        'Article One',
+        'body':
+        'sdjflkds flsjflkds jfdslkjflkdsjf ldsjflks jflkdsjlkf dslkfjslkfjds flksjflksdj fldsjflksdj lfdskj flkdsjf ldsjflkdsjf lks jflkdsj flks jflkdsjf lksjf lkds jflds jf slk jflks jflj slj lfds',
+        'author':
+        'Brad',
+        'create_date':
+        '04-25-2017'
+    }, {
+        'id':
+        2,
+        'title':
+        'Article Two',
+        'body':
+        'sdjflkds flsjflkds jfdslkjflkdsjf ldsjflks jflkdsjlkf dslkfjslkfjds flksjflksdj fldsjflksdj lfdskj flkdsjf ldsjflkdsjf lks jflkdsj flks jflkdsjf lksjf lkds jflds jf slk jflks jflj slj lfds',
+        'author':
+        'Brad',
+        'create_date':
+        '04-28-2017'
+    }, {
+        'id':
+        1,
+        'title':
+        'Article Three',
+        'body':
+        'sdjflkds flsjflkds jfdslkjflkdsjf ldsjflks jflkdsjlkf dslkfjslkfjds flksjflksdj fldsjflksdj lfdskj flkdsjf ldsjflkdsjf lks jflkdsj flks jflkdsjf lksjf lkds jflds jf slk jflks jflj slj lfds',
+        'author':
+        'Brad',
+        'create_date':
+        '04-2925-2017'
+    }]
+    return Articles
